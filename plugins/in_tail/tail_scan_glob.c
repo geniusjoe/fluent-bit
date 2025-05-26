5514f4e0bcb65dfc9c3ad21b2832c7fd712bfa8c
ninjazhou <843520313@qq.com>
2025-05-26 12:53:33 +0800

    remove GLOB_ERR flag in tail input plugin glob function

--- plugins/in_tail/tail_scan_glob.c
+++ plugins/in_tail/tail_scan_glob.c
@@ -201,7 +201,7 @@ static int tail_scan_path(const char *path, struct flb_tail_config *ctx)
     globbuf.gl_pathv = NULL;
 
     /* Scan the given path */
-    ret = do_glob(path, GLOB_TILDE | GLOB_ERR, NULL, &globbuf);
+    ret = do_glob(path, GLOB_TILDE, NULL, &globbuf);
     if (ret != 0) {
         switch (ret) {
         case GLOB_NOSPACE:
